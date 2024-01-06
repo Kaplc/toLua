b501a1deae435984827fa27471f2b411848cd42b
Kaplc <755536175@qq.com>
2024-01-06 22:57:34 +0800

    初始化仓库

--- Assets/Scripts/ToLuaManager/ToLuaCustomLoader.cs
+++ Assets/Scripts/ToLuaManager/ToLuaCustomLoader.cs
@@ -1,8 +1,23 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using LuaInterface;
 using UnityEngine;
 
-public class ToLuaCustomLoader : MonoBehaviour
+public class ToLuaCustomLoader : LuaFileUtils
 {
-    
+    public override byte[] ReadFile(string fileName)
+    {
+        byte[] buffer = null;
+
+        if (!fileName.EndsWith(".lua"))
+        {
+            fileName += ".lua";
+        }
+
+        string[] strs = fileName.Split('/');
+        
+        // TextAsset luaCode = 
+
+        return null;
+    }
 }
